ea7706a9bcdab16ba150d401f7110a4ee8a82c85
Valensius Alven <valensius24001@mail.unpad.ac.id>
2026-05-24 21:06:32 +0700

    fix: enable PWA support in next.config.mjs and prepare for GitHub-Vercel integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
